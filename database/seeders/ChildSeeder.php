4346a9e2020a9b72a59bd2e1829fa05deac4a6f5
eyaberr <eyaberriche@gmail.com>
2023-04-10 10:05:20 +0100

    all seeders

---
+++ database/seeders/ChildSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ChildSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
