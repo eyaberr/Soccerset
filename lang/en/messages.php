4b621b3e106785d663b289f7946319fd66111131
eyaberr <eyaberriche@gmail.com>
2023-04-13 13:58:40 +0100

    lang modify

---
+++ lang/en/messages.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'edit_user_title' => 'Edit & Update User',
+    'back_button' => 'Back',
+    'add_user_title'=>'ADD USER',
+    'users_space_title'=>'USERS SPACE',
+    'user_id'=>'#',
+    'user_name'=>'Name',
+    'user_email'=>'Email',
+    'user_role'=>'Role',
+    'show_button'=>'Show',
+    'edit_button'=>'Edit',
+    'delete_button'=>'Delete',
+    'create_user_title'=>'CREATE USER',
+    'user_password'=>'Password',
+    'user_confirm_password'=>'Password Confirmation',
+    'submit_button'=>'Submit',
+    'show_user_title'=>'Show User',
+
+];
