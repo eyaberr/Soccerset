03af12cfc6823871ad631ca7b160325946abbd72
eyaberr <eyaberriche@gmail.com>
2023-05-08 11:28:45 +0100

    child index and create

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,9 +39,9 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ],
     ];
 
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ChildController;
 use App\Http\Controllers\Api\EventController;
 use App\Http\Controllers\Api\GroupController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,6 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('events', EventController::class);
-Route::apiResource('groups', GroupController::class);
-Route::apiResource('children', ChildController::class);
+//Route::apiResource('events', EventController::class);
+//Route::apiResource('groups', GroupController::class);
+//Route::apiResource('children', ChildController::class);
+Route::post('login',[UserController::class,'loginUser']);
+
+Route::post('/auth/register', [AuthController::class, 'createUser']);
+Route::post('/auth/login', [AuthController::class, 'loginUser']);
