8083e174cc8b22ca95ae662daa99b58382dfc5f1
eyaberr <eyaberriche@gmail.com>
2023-05-16 10:14:33 +0100

    update stats api DONE

--- app/Http/Controllers/Api/UpdateStatsController.php
+++ app/Http/Controllers/Api/UpdateStatsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use App\Models\EventSubscription;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,17 +17,31 @@ class UpdateStatsController extends Controller
     public function __invoke(Request $request, $id)
     {
         $user = Auth::user();
+        $rules = ['stats' => 'required|array'];
         if ($user->role_id === User::ROLES['trainer']) {
-            $request->validate([
-                'stats' => 'required|array'
-            ]);
-            $stats = $request->input('stats');
-
             $eventSubscription = EventSubscription::whereHas('event', function ($query) use ($user) {
                 $query->where('user_id', $user->id);
-            })->findOrFail($id);
+            })->with('event')->findOrFail($id);
 
-            $eventSubscription->stats = $stats;
+            switch ($eventSubscription->event->type) {
+                case Event::TYPES['training']:
+                    $rules['stats.goals'] = 'required|integer';
+                    $rules['stats.touches'] = 'required|integer';
+                case Event::TYPES['friendly_game']:
+                    $rules['stats.attack'] = 'required|integer';
+                    $rules['stats.defence'] = 'required|integer';
+                    $rules['stats.dribble'] = 'required|integer';
+                    break;
+                case Event::TYPES['medical_checkup']:
+                    $rules['stats.heart_rate'] = 'required|integer';
+                    $rules['stats.blood_pressure'] = 'required|integer';
+                    $rules['stats.height'] = 'required|integer';
+                    $rules['stats.weight'] = 'required|integer';
+                    break;
+            }
+
+            $request->validate($rules);
+            $eventSubscription->stats = $request->input('stats');
             $eventSubscription->save();
             return response()->json($eventSubscription);
         }
